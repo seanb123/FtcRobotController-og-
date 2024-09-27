441ae81467c9a9090c91a403b5647aa3f6dae2f1
ryan <ryanzhou63@gmail.com>
2024-09-26 21:35:07 -0400

    0.5

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleop/teleop.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class teleop extends LinearOpMode {
+    @Override
+    public void runOpMode(){
+
+    }
+}
