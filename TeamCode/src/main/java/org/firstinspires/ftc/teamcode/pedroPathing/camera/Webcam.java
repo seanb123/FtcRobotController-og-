ec74b43f59ac7f485127841935ec22195f2ef8ce
Themika <Weerasuriyathemik@gmail.com>
2024-10-07 17:59:33 -0400

    Added a way to use yellow pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/camera/Webcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/camera/Webcam.java
@@ -13,13 +13,11 @@ public class Webcam extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        // Initialize the webcam and pipeline
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
         yellow pipeline = new yellow(telemetry);
         webcam.setPipeline(pipeline);
 
-        // Start the camera stream
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
@@ -28,36 +26,29 @@ public class Webcam extends LinearOpMode {
 
             @Override
             public void onError(int errorCode) {
-                // Handle camera errors
             }
         });
 
-        // Wait for the start button to be pressed
         waitForStart();
 
-        // Get the detected location of the object
         yellow.Location location = pipeline.getLocation();
 
-        // Perform actions based on the detected location
         switch (location) {
             case LEFT:
-                // Code to execute if the object is detected on the lefttelemetry.addData("Object Location", "Left");
-                // ... your autonomous logic here ...
+                // Code to execute if the object is detected on the left
+                telemetry.addData("Object Location", "Left");
                 break;
             case RIGHT:
                 // Code to execute if the object is detected on the right
                 telemetry.addData("Object Location", "Right");
-                // ... your autonomous logic here ...
                 break;
             case MIDDLE:
                 // Code to execute if the object is detected in the middle
                 telemetry.addData("Object Location", "Middle");
-                // ... your autonomous logic here ...
                 break;
             case NOT_FOUND:
                 // Code to execute if the object is not found
                 telemetry.addData("Object Location", "Not Found");
-                // ... your autonomous logic here ...
                 break;
         }
 
