d28ba795b6db9ffc088dede1460c44f2bd1866a1
seanb123 <seanboretzky1907@gmail.com>
2024-11-12 20:50:09 -0500

    te

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/autonomous/path1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/autonomous/path1.java
@@ -8,6 +8,7 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 
@@ -24,6 +25,7 @@ import org.firstinspires.ftc.teamcode.pedroPathing.subsystem.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.pedroPathing.subsystem.RotateSlideSubsystem;
 import org.firstinspires.ftc.teamcode.pedroPathing.subsystem.SlideSubsystem;
 
+
 @Config
 @Autonomous
 public class path1 extends CommandOpMode {
@@ -38,4 +40,25 @@ public class path1 extends CommandOpMode {
         rotate_slide_subsystem = new RotateSlideSubsystem(hardwareMap);
 
     }
+    // actionBuilder builds from the drive steps passed to it
+    @Override
+    public void runOpMode() {
+        // instantiate your MecanumDrive at a particular pose.
+        Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        // actionBuilder builds from the drive steps passed to it
+        TrajectoryActionBuilder myBot = drive.actionBuilder(initialPose)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+
+
+                        .forward(30)
+                        .turn(Math.toRadians(90))
+                        .forward(2)
+                        //spin intake to pick up brick
+                        .strafeLeft(26)
+                        .forward(20)
+                        .turn(Math.toRadians(45))
+                        //put brick into basket
+
+                        .build();
 }
