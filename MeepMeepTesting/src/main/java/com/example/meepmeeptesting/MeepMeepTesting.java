72ee08b4cb54768dc892f931e6947400ace58e5c
ryan <ryanzhou63@gmail.com>
2024-11-16 08:16:41 -0500

    RR Actions 1.0

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -10,24 +10,17 @@ import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
-        Pose2d initialPose = new Pose2d(-35, -60, Math.toRadians(90));
+        Pose2d initialPose = new Pose2d(16, -60, Math.toRadians(90));
         RoadRunnerBotEntity redPathLeft = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(initialPose)
-                        .strafeTo(new Vector2d(-60, -60))
-                        .strafeTo(new Vector2d(-58, -56))
+                        .strafeTo(new Vector2d(48, -58))
+                        .strafeTo(new Vector2d(48, -35))
+                        .strafeTo(new Vector2d(-48, -35))
                         .strafeTo(new Vector2d(-54, -53))
                         .turn(Math.toRadians(135))
-                        .waitSeconds(1)
-                        .strafeTo(new Vector2d(-58, -56))
-                        .waitSeconds(0.5)
-                        .turn(Math.toRadians(-135))
-                        .strafeTo(new Vector2d(-48, -40))
-                        .waitSeconds(1)
-                        .strafeTo(new Vector2d(-54, -53))
-                        .turn(Math.toRadians(135))
-                        .waitSeconds(3)
+
                         .build());
 
 
