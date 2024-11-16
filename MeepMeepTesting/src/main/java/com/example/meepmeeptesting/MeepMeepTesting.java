4989f73ba912f368cecb8ac581ef18d01f26c137
ryan <ryanzhou63@gmail.com>
2024-11-15 21:29:26 -0500

    RR Actions 1.0

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
@@ -9,24 +10,24 @@ import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
-        Pose2d initialPose = new Pose2d(12, -68, Math.toRadians(90));
+        Pose2d initialPose = new Pose2d(-35, -60, Math.toRadians(90));
         RoadRunnerBotEntity redPathLeft = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(initialPose)
-                        .forward(30)
-                        .strafeLeft(60)
-                        .turn(Math.toRadians(145))
-                        .forward(15)
-                        .strafeLeft(10)
-                        .back(30)
-                        .turn(Math.toRadians(-75))
-                        .turn(Math.toRadians(75))
-                        .forward(30)
-                        .strafeRight(10)
-                        .turn(Math.toRadians(-145))
-                        .strafeRight(115)
-                        .back(15)
+                        .strafeTo(new Vector2d(-60, -60))
+                        .strafeTo(new Vector2d(-58, -56))
+                        .strafeTo(new Vector2d(-54, -53))
+                        .turn(Math.toRadians(135))
+                        .waitSeconds(1)
+                        .strafeTo(new Vector2d(-58, -56))
+                        .waitSeconds(0.5)
+                        .turn(Math.toRadians(-135))
+                        .strafeTo(new Vector2d(-48, -40))
+                        .waitSeconds(1)
+                        .strafeTo(new Vector2d(-54, -53))
+                        .turn(Math.toRadians(135))
+                        .waitSeconds(3)
                         .build());
 
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/autonomous/Bluetest.java
@@ -0,0 +1,181 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.autonomous;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+
+@Config
+@Autonomous(name = "blue test", group = "Autonomous")
+public class Bluetest extends LinearOpMode {
+    private MecanumDrive drive;
+    private Pose2d beginPose;
+    private DcMotor rotateMotor, slideMotor;
+    private CRServo intakeServo;
+
+
+    // actionBuilder builds from the drive steps passed to it
+    @Override
+    public void runOpMode() {
+        rotateMotor = hardwareMap.get(DcMotorEx.class, "rotateMotor");
+        rotateMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rotateMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rotateMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        slideMotor = hardwareMap.get(DcMotorEx.class, "slideMotor");
+        slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slideMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        slideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
+
+        beginPose = new Pose2d(0, 0, 0);
+        drive = new MecanumDrive(hardwareMap, beginPose);
+
+        Pose2d initialPose = new Pose2d(-35, -60, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        //1  Block
+        Action firstTrackPhase1 = drive.actionBuilder(drive.pose)
+                .strafeTo(new Vector2d(-60, -60))
+                .strafeTo(new Vector2d(-58, -56))
+                .strafeTo(new Vector2d(-54, -53))
+                .turn(Math.toRadians(135))
+                .stopAndAdd(new Arm(rotateMotor, 1250))
+                .waitSeconds(1)
+                .stopAndAdd(new Slide(slideMotor, 1750))
+                .strafeTo(new Vector2d(-58, -56))
+                .waitSeconds(0.5)
+                .stopAndAdd(new Intake(intakeServo, 0))
+                .waitSeconds(1)
+                .stopAndAdd(new Intake(intakeServo, 1))
+                .stopAndAdd(new Slide(slideMotor, 0))
+                .waitSeconds(2)
+                .stopAndAdd(new Arm(rotateMotor, 0))
+                .waitSeconds(1)
+                .turn(Math.toRadians(-135))
+                .strafeTo(new Vector2d(-48, -40))
+                .stopAndAdd(new SequentialAction(
+                        new Arm(rotateMotor, 200),
+                        new Slide(slideMotor, 1000)
+                ))
+                .waitSeconds(1.5)
+                .strafeTo(new Vector2d(-54, -53))
+                .turn(Math.toRadians(135))
+                .stopAndAdd(new Slide(slideMotor, 0))
+                .waitSeconds(0.5)
+                .stopAndAdd(new Arm(rotateMotor, 1250))
+                .waitSeconds(1)
+                .stopAndAdd(new Slide(slideMotor, 1750))
+                .strafeTo(new Vector2d(-58, -56))
+                .waitSeconds(1)
+                .stopAndAdd(new Intake(intakeServo, 0))
+                .waitSeconds(1)
+                .stopAndAdd(new Slide(slideMotor, 0))
+                .waitSeconds(2)
+                .stopAndAdd(new Arm(rotateMotor, 0))
+                .waitSeconds(1)
+                .build();
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        Actions.runBlocking(new SequentialAction(
+                        firstTrackPhase1
+                )
+        );
+
+    }
+    // -------------------------Rotate Arm-------------------------
+    private class Arm implements Action {
+        DcMotor rotateMotor;
+        int position;
+
+        public Arm(DcMotor motor, int position){
+            this.rotateMotor = motor;
+            this.position = position;
+        }
+
+        public void goto_position(){
+            rotateMotor.setTargetPosition(position);
+            rotateMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rotateMotor.setPower(0.8);
+        }
+
+        public boolean reached_position(){
+            return Math.abs(position - rotateMotor.getCurrentPosition()) <= 50;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            goto_position();
+            return reached_position();
+        }
+    }
+
+    // -------------------------Linear Slides-------------------------
+    private class Slide implements Action {
+        DcMotor slideMotor;
+        int position;
+
+        public Slide(DcMotor motor, int position){
+            this.slideMotor = motor;
+            this.position = position;
+        }
+
+        public void goto_position(){
+            slideMotor.setTargetPosition(position);
+            slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            slideMotor.setPower(0.5);
+        }
+
+        public boolean reached_position(){
+            return Math.abs(position - slideMotor.getCurrentPosition()) <= 50;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            goto_position();
+            return reached_position();
+        }
+    }
+
+    // -------------------------INTAKE-------------------------
+    private class Intake implements Action {
+        CRServo intakeServo;
+        int direction;
+        ElapsedTime timer;
+
+        public Intake(CRServo servo, int dir){
+            this.intakeServo = servo;
+            this.direction = dir;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            if (timer == null){
+                timer = new ElapsedTime();
+            }
+            intakeServo.setPower(direction == 1 ? 1 : -0.7);
+
+            return timer.seconds() < 1 ? true : false;
+        }
+    }
+
+}
