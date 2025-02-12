e3633a450d666ba92df4db9c7140a3a75450bb6c
Themika <88339133+Themika@users.noreply.github.com>
2025-02-12 16:28:51 -0500

    PATHS 2

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,9 +1,9 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -18,41 +18,43 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myFirstBot.runAction(myFirstBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+        myFirstBot.runAction(myFirstBot.getDrive().actionBuilder(new Pose2d(10.6, -60.1, Math.toRadians(90)))
+                .strafeTo(new Vector2d(38,-37))
+                .strafeTo(new Vector2d(38,-12))
+                .strafeTo(new Vector2d(47,-12))
+                .strafeTo(new Vector2d(47,-55))
+                .strafeTo(new Vector2d(47,-12))
+                .strafeTo(new Vector2d(58,-12))
+                .strafeTo(new Vector2d(58,-55))
+                .strafeTo(new Vector2d(58,-12))
+                .strafeTo(new Vector2d(62,-12))
+                .strafeTo(new Vector2d(62,-55))
+                .strafeTo(new Vector2d(58,-60))
+                .turn(Math.toRadians(180))
+                .strafeTo(new Vector2d(0,-40))
+                .turn(Math.toRadians(180))
+                .strafeTo(new Vector2d(0,-33))
+                .turn(Math.toRadians(180))
+                .strafeTo(new Vector2d(58,-60))
+                .turn(Math.toRadians(180))
+                .strafeTo(new Vector2d(0,-40))
+                .turn(Math.toRadians(180))
+                .strafeTo(new Vector2d(0,-33))
+
+
+
+
+
+
+                //Pick up piece
+
                 .build());
 
-        // Declare out second bot
-        RoadRunnerBotEntity mySecondBot = new DefaultBotBuilder(meepMeep)
-                // We set this bot to be red
-                .setColorScheme(new ColorSchemeRedDark())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-
-        mySecondBot.runAction(mySecondBot.getDrive().actionBuilder(new Pose2d(30, 30, Math.toRadians(180)))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .build());
-
-        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_OFFICIAL)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(1.0f)
                 // Add both of our declared bot entities`
                 .addEntity(myFirstBot)
-                .addEntity(mySecondBot)
                 .start();
     }
 }
